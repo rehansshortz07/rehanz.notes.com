a160ff9157aef5ceee1ee3cc62d91e92e38857cb
shaik rehan <rehansshortz.07@gmail.com>
2026-09-10 15:09:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "uninotes-cache-v7"; // Increment cache version to force a clean update
+const CACHE_NAME = "TechVault-v7"; // Increment cache version to force a clean update
 const ASSETS_TO_CACHE = [
   "./index.html",
   "./style.css",
